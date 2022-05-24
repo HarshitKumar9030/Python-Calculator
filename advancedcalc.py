81850673d9d3223b2d69e5e884a09746deb179e4
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2022-05-24 22:22:21 +0530

    Added Functions

--- advancedcalc.py
+++ advancedcalc.py
@@ -7,3 +7,32 @@ screenSize = "279x442"
 window.geometry(screenSize)
 window.resizable(0, 0)
 window.title("Calcualtor")
+
+def about():
+    messagebox.showinfo('About',"\n \n \n   Made by Harshit kumar \n \n INSTAGRAM : @harshit_kumarofficial \n \n Github : harshitkumar9030 \n \n Twitter : @OhHarshit \n \n")
+
+def clickButton(item):
+    global expression
+    inputText.set(inputText.get()+(str(item)))
+
+def clearButton():
+    global expression
+    expression = ""
+    inputText.set(inputText.get()[0:-1])
+
+def clearAll():
+    inputText.set("")
+
+def expand():
+    if screenSize=="279x500":
+        window.geometry("279x555")
+    else:
+        window.geometry("279x555")
+def equalButton():
+    result = ""
+    try:
+        result = eval(inputText.get())
+        inputText.set(result)
+    except:
+        result = "ERROR..."
+        inputText.set(result)
