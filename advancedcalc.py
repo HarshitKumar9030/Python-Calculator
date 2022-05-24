0b9540d2147b30e2b3c96ae7ac0172f6c2df35fa
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2022-05-24 22:20:49 +0530

    Created a window

--- advancedcalc.py
+++ advancedcalc.py
@@ -1,3 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
 from math import pi, e, sin, cos, tan, log
+
+window = Tk()
+screenSize = "279x442"
+window.geometry(screenSize)
+window.resizable(0, 0)
+window.title("Calcualtor")
